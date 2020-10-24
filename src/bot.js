23011e45fef4c7c0c8800009fef072b6e76b0657
Andrew Westgerdes <ajwestgerdes@gmail.com>
2020-10-24 00:08:03 -0500

    added 8ball functionality still working out kinks

--- src/bot.js
+++ src/bot.js
@@ -21,6 +21,33 @@ client.on('message', (message) => {
     }
 })
 
+//check for !8ball then return 8ball statement based on question asked
+client.on('message', (message) => {
+    if (message.content.startsWith(prefix)) {
+        const [command, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
+        var eball = Math.floor(Math.random() * 1) + 1;
+        console.log(args)
+
+        //if logic for 8ball
+        if (command === '8ball' && eball === 1) {
+            message.channel.send(...args)
+            message.channel.send('8ball says phrase 1')
+        } else if (command === '8ball' && eball === 2) {
+            message.channel.send('8ball says phrase 2')
+        } else if (command === '8ball' && eball === 3) {
+            message.channel.send('8ball says phrase 3')
+        } else if (command === '8ball' && eball === 4) {
+            message.channel.send('8ball says phrase 4')
+        } else if (command === '8ball' && eball === 5) {
+            message.channel.send('8ball says phrase 5')
+        } else if (command === '8ball' && eball === 6) {
+            message.channel.send('8ball says phrase 6')
+        }
+        //delete !8ball from message board
+        message.delete();
+    }
+})
+
 //login bot
 client.login(process.env.BOT_TOKEN);
 

1aaf21174575b220f1064c3adba5faef48cf485c
Andrew Westgerdes <ajwestgerdes@gmail.com>
2021-07-16 21:21:38 -0500

    updated stopwatch logic

--- src/commands/stopwatch.js
+++ src/commands/stopwatch.js
@@ -1,13 +1,31 @@
 export function stopwatch(message, client) {
-    console.log(message)
-    console.log(message.author)
+    
     let mentionId = message.content.split('@!')[1].slice(0, -1)
     let msgAuther = message.author.username
-    console.log(mentionId)
     let mentionsMap = message.mentions.users
-    console.log(mentionsMap.get(mentionId))
+   
+    
+    // console.log(mentionsMap)
+ 
 
-    client.users.fetch(mentionId, false).then((user) => {
-        user.send('ATTENTION ' + user.username + '! \n' + msgAuther + ' has put you on the clock! Better join there channel as fast as you can! Or be publicly ridiculed.');
-       });
+    // client.users.fetch(mentionId, false).then((user) => {
+    //     user.send('ATTENTION ' + user.username + '! \n' + msgAuther + ' has put you on the clock! Better join there channel as fast as you can! Or be publicly ridiculed.');
+    //    });
+
+    client.on('voiceStateUpdate', (oldState, newState) => {
+        console.log('made it inside voice state update')
+        console.log(oldState)
+        console.log('------------------------------------------')
+        console.log(oldState.guild)
+
+        let newUserChannel = newMember.voiceChannel
+        let oldUserChannel = oldMember.voiceChannel
+      
+      
+        if(oldUserChannel === undefined && newUserChannel !== undefined) {
+      
+           // User Joins
+      
+        }
+    })
 }
